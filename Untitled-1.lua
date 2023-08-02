6bd4a9c319012036bb538de4b35ff6009240cf35
Scripterman235435 <141252893+Scripterman235435@users.noreply.github.com>
2023-08-02 14:14:28 -0700

    Add files via upload

---
+++ Untitled-1.lua
@@ -0,0 +1,67 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("ONI HUB", "DarkTheme")
+
+-- MAIN
+local Main = Window:NewTab("GUI")
+local MainSection = Main:NewSection("GUI'S")
+
+
+
+MainSection:NewButton("Inf Yield", "INF YIELD", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end)
+
+
+MainSection:NewButton("RO-Ghoul GUI", "loads a op gui", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/2uF3iQAf', true))()
+end)
+
+
+MainSection:NewButton("Blox Fruit GUI(KEY)", "Loads Op GUI", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()
+end)
+
+
+MainSection:NewButton("Veichle Legends", "OP GUI", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IExpIoit/Script/main/UltimateHub"))()
+end)
+
+
+MainSection:NewButton("Arm Wrestle Sim", "Op GUI", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iceplayz45/awspublic/main/.gitignore", true))()
+end)
+
+
+MainSection:NewButton("Project Slayers", "OP GUI", function()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/ImperorLegend/Shark-Hub-v1/main/scriptt"),true))()
+end)
+
+
+MainSection:NewButton("Murder Mystery 2", "OP GUI", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vwSaraa/LunarHub/main/mm2"))()
+end)
+
+
+MainSection:NewButton("Speed Legends", "OP GUI", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/1iMHrZ50", true))()
+end)
+
+
+MainSection:NewButton("DeepWoken", "OP GUI", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/w8qQ07Pm", true))()
+end)
+
+
+MainSection:NewButton("KAT", "GUI", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+end)
+
+
+MainSection:NewButton("Rush Point", "OP GUI", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/noobscripter38493/Rush-point/main/RUSH%20POINT!!!!!.lua"))()
+end)
+
+
+MainSection:NewButton("Energy Assault", "OP GUI", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/rblxscriptsnet/unfair/main/rblxhub.lua'),true))()
+end)
